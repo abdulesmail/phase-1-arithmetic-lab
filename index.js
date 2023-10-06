78663b4005ce62786bc3de28375385ad75e7a47b
abdulesmail <abdul.esmail@student.moringaschool.com>
2023-10-06 10:33:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ let num2 = 2
 let multiply =num1 * num2
 
 
- const random = math.floor(math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+ let random = Math.floor(Math.random() * 100)
 
 
-let num3 = 20;
-let num4 = 4;
-let mod = (num3 % num4 === 4) ? 4 : -1;
+let num3 = 204;
+let num4 = 10;
+let mod = num3 % num4
 
-var numbers = [ 10, 5, 15, 20, 7, 12];
-var max = Math.max.apply(null, numbers);
+
+let max = Math.max(15,17,18,19,20);
+
 
 
 
